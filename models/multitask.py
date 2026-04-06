5858acf13cd40aff66e958dd6338375660909732
Soumya Ranjan Patel <patelsoumyaranjan04@gmail.com>
2026-04-06 16:05:34 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -13,7 +13,7 @@ from .segmentation import VGG11UNet
 # Google Drive file IDs — REPLACE these with your actual IDs
 DRIVE_IDS = {
     "classifier": "1XHUF72fPxKJKHC6v3ZxPxTBBwvdv3F-c",
-    "localizer": "1LB90aAeEF-9CuEILdAV9-QwEzBvlZdQN",
+    "localizer": "16zjN4oq_PLIkV0AE_jhl7axLKjAkSmzP",
     "unet": "<unet.pth drive id>",
 }
 
