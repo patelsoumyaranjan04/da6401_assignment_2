a88c60397202342b0869ed079ca1395ba0d3f571
Soumya Ranjan Patel <patelsoumyaranjan04@gmail.com>
2026-04-09 19:48:37 +0530

    Update multitask.py

--- models/multitask.py
+++ models/multitask.py
@@ -14,7 +14,7 @@ from .segmentation import VGG11UNet
 DRIVE_IDS = {
     "classifier": "1XHUF72fPxKJKHC6v3ZxPxTBBwvdv3F-c",
     "localizer": "1WNu0Nb6ScgIrfZU-DUM3yZ7RJimgdDAb",
-    "unet": "1WGzgE6uNbBljSLuZBIgtkS89UlP-uhSY",
+    "unet": "1YXtggaXDYd73XlKYf_piOJJE5bbN0IL2",
 }
 
 
